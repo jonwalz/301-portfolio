f459d52ec67886c33964d93a612eadf2390396a7
Jon Walz <jon.walz.music@gmail.com>
2016-08-27 15:45:41 -0700

    Constructor works :

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-var portfolioPieces = [];
+var portfolioBlocks = [];
 
 function Piece(keys) {
-    // Title, Date, Url, Github url, Description
+    // Title, Date, Url, Github url, Description, imgUrl
     this.title = keys.title;
     this.date = keys.date;
     this.url = keys.url;
     this.gitUrl = keys.gitUrl;
     this.description = keys.description;
+    this.imgUrl = keys.imgUrl;
 }
 
-Piece.prototype.toHtml = function(){
-    var $newPiece = $('')
+Piece.prototype.postIt = function(){
+    var $newPiece = $('#templateDiv').clone(); //this creates a clone to save all styling and formatting
+
+    $newPiece.find(".pieceTitle").html(this.title);
+    $newPiece.find(".description p").html(this.description);
+    $newPiece.append('<hr>');
+
+    $newPiece.removeClass('template');
+
+    return $newPiece;
 };
+
+portfolioPieces.forEach(function(el){
+    portfolioBlocks.push(new Piece(el))
+});
+
+portfolioBlocks.forEach(function(p){
+   $("#templateDiv").parent().append(p.postIt());
+});
+
---
+++ js/portfolioData.js
@@ -0,0 +1,26 @@
+var portfolioPieces = [
+    {
+        title: "My first portfolio piece",
+        date: "2106-08-15",
+        url: "www.jonathanwalz.com",
+        gitUrl: "https://www.github.com",
+        Description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows"
+
+    },
+    {
+        title: "My Second portfolio piece",
+        date: "2106-08-15",
+        url: "www.jonathanwalz.com",
+        gitUrl: "https://www.github.com",
+        Description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows"
+
+    },
+    {
+        title: "My Third portfolio piece",
+        date: "2106-08-15",
+        url: "www.jonathanwalz.com",
+        gitUrl: "https://www.github.com",
+        Description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows"
+
+    }
+];
