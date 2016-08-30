d91a8fdf06f461dbd21ea86d54152a3e0719624d
Jon Walz <jon.walz.music@gmail.com>
2016-08-29 20:40:21 -0700

    Added gulp and npm

--- js/portfolioView.js
+++ js/portfolioView.js
@@ -1,24 +1,28 @@
 var portfolioView = {};
 
 portfolioView.initialize = function () {
-  $('#aboutMe').hide();
+    $('#aboutMe').hide();
+    $('#contact').hide();
 };
 
 portfolioView.navViews = function() {
     $('.main-nav').on('click', function(e){
         e.preventDefault();
 
+        $('section')
+            .not(this)
+            .fadeOut(500);
+        console.log(this);
+        $(this).fadeIn(500);
+
+
         // change display of nav bar
         if(e.target.textContent == "About") {
 
             // change display of content
-            $('section')
-                .find('div')
-                .not('#aboutMe')
-                .addClass('no-display');
 
-            $('#aboutMe')
-                .fadeIn(500);
+        } else if (e.target.textContent == "Work") {
+
         }
 
     })
