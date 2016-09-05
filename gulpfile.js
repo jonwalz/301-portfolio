4e8943a7a442f36a15949e220cd1fa67d0328921
Jon Walz <jon.walz.music@gmail.com>
2016-09-05 14:41:52 -0700

    Adjusting fluid styles

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,17 @@ var image = require('gulp-image');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 
+// STYLE FILES
+
+var cssFiles = [
+    "./dev/vendor/normalize.css",
+    "./dev/vendor/skeleton.css",
+    "./dev/css/*.scss"
+];
+
 
 gulp.task('sass', function () {
-    return gulp.src('./dev/css/*.scss')
+    return gulp.src(cssFiles)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
@@ -23,14 +31,14 @@ gulp.task('sass', function () {
 });
 
 gulp.task('index', function () {
-   return gulp.src('./dev/index.html')
-       .pipe(gulp.dest('./dist'));
+    return gulp.src('./dev/index.html')
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js', function () {
-   return gulp.src('./dev/js/*.js')
-       .pipe(uglify())
-       .pipe(gulp.dest('./dist/js'));
+    return gulp.src('./dev/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('compress', function (cb) {
@@ -44,25 +52,25 @@ gulp.task('compress', function (cb) {
 });
 
 gulp.task('bower', function () {
-   return gulp.src(
-       [
-           './bower_components/Flowtype.js/flowtype.js',
-           './bower_components/jquery/dist/jquery.min.js'
-       ])
-       .pipe(gulp.dest('./dist/vendor/'))
+    return gulp.src(
+        [
+            './bower_components/Flowtype.js/flowtype.js',
+            './bower_components/jquery/dist/jquery.min.js'
+        ])
+        .pipe(gulp.dest('./dist/vendor/'))
 });
 
 gulp.task('images', function () {
-   return gulp.src('./dev/img/*')
-       .pipe(image())
-       .pipe(gulp.dest('./dist/img'));
+    return gulp.src('./dev/img/*')
+        .pipe(image())
+        .pipe(gulp.dest('./dist/img'));
 });
 
-gulp.task('default', ['sass', 'images', 'js'], function() {
+gulp.task('default', ['sass', 'images', 'js'], function () {
     return gulp.src('./dev/css/*.css');
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('./dev/css/*.scss', ['sass']);
     gulp.watch(["./dev/js/*.js"], ['js'])
 });
