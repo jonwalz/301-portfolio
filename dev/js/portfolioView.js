71f63449bdcb0c0cad420890f8e6b04af96ec886
Jon Walz <jon.walz.music@gmail.com>
2016-09-04 21:20:59 -0700

    Fixed navbar issues

--- dev/js/portfolioView.js
+++ dev/js/portfolioView.js
@@ -1,28 +1,45 @@
 var portfolioView = {};
 
 portfolioView.initialize = function () {
-    // $('#aboutMe').hide();
-    // $('#contact').hide();
+    $('#aboutMe').hide();
+    $('#contact').hide();
     // $('div[data-article="articles"]').fadeIn(300);
     console.log('hit');
 };
 
 portfolioView.navViews = function() {
+
     $('.main-nav').on('click', function(e){
         e.preventDefault();
 
-        $('section > div')
-            .hide(500, function(){
-                if(e.target.textContent == "About") {
-                    $('#aboutMe').toggle();
-                }
-                if (e.target.textContent == "Work") {
-                    $('div[data-article="articles"]').toggle();
-                }
-                if (e.target.textContent == "Contact") {
-                    $('#contact').fadeIn(300);
-                }
-            });
+
+        if(e.target.text == "About") {
+            $('section > div').slideUp(300);
+            $('#aboutMe')
+                .delay(200)
+                .hide()
+                .slideDown(300);
+        }
+        if(e.target.text == "Work") {
+            $('section > div').slideUp(300);
+            $("div[data-article='articles']")
+                .delay(200)
+                .hide()
+                .each(function (i) {
+                    $(this)
+                        .delay(200*i)
+                        .slideDown(300);
+            })
+        }
+
+        if(e.target.text == "Contact") {
+            $('section > div').slideUp(300);
+            $('#contact').animate({
+                left: "+=50",
+                height: "toggle"
+            }, 300);
+        }
+
     })
 };
 
