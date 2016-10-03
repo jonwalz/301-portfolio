929eb1eb268088fcce9ba9669bc7aafda05ff3ee
Jon Walz <jon.walz.music@gmail.com>
2016-10-02 22:14:57 -0700

    Minor styling, working on page.js

--- dist/js/contactController.js
+++ dist/js/contactController.js
@@ -1,17 +1,1 @@
-(function (module) {
-    var contactController = {};
-
-    // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
-    contactController.index = function () {
-        $('section > div').slideUp(300);
-        $("#contact")
-            .delay(200)
-            .hide()
-            .each(function (i) {
-                $(this)
-                    .delay(200 * i)
-                    .slideDown(300);
-            })
-    };
-    module.contactController = contactController;
-})(window);
+!function(n){var i={};i.index=function(){$("section > div").slideUp(300),$("#contact").delay(200).hide().each(function(n){$(this).delay(200*n).slideDown(300)})},n.contactController=i}(window);
